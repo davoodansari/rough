470479b5f16b6f0aca84a8ce3be3437dcd21bc57
davoodansari <david.ansari@gmail.com>
2010-09-16 18:43:27 -0400

    first commit

---
+++ CMakeFiles/rough.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/rough.dir/main.cpp.o"
+  "rough.pdb"
+  "rough"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/rough.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
